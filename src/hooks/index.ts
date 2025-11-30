0403c9b9f8572afd0e22e43e5b2fd245c0a463ae
Stoyan Kostadinov <stoyan.kostadinov85@gmail.com>
2025-12-01 01:10:14 +0200

    feat: export all new hooks from index.ts
    - Added exports for all 7 new refactored hooks
    - Hooks now available for gradual integration
    - Build passing ✅

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,3 +4,12 @@ export { useAudio } from "./useAudio";
 export { useKeyboardInput, useSwipeInput } from "./useInput";
 export { useGameState } from "./useGameState";
 
+// new refactored hooks (available for gradual integration)
+export { useLevelState } from "./useLevelState";
+export { useInventoryState } from "./useInventoryState";
+export { useGameLifecycle } from "./useGameLifecycle";
+export { useBombMechanics } from "./useBombMechanics";
+export { useCloakMechanics } from "./useCloakMechanics";
+export { useWolfState } from "./useWolfState";
+export { usePlayerState } from "./usePlayerState";
+
